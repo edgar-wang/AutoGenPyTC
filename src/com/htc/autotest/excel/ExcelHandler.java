2a17a0494dfbf396458bac2a8c7857e95d89a20e
Edgar_Wang <edgarwang@gmail.com>
2015-10-15 18:17:23 +0800

    fix nullpointer exception when visit cell

--- src/com/htc/autotest/excel/ExcelHandler.java
+++ src/com/htc/autotest/excel/ExcelHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -43,13 +44,26 @@ public class ExcelHandler {
 			int count = 0;
 			for (int i = 1; i < sheet.getPhysicalNumberOfRows(); i++) {
 				     row = sheet.getRow(i); // get ith Row
-				     String title = row.getCell(Constants.XLS_TITLE).toString().trim();
-				     String type = row.getCell(Constants.XLS_TYPE).toString().trim();
-				     String priority = row.getCell(Constants.XLS_PRIORITY).toString().trim();
-				     String excepted_result = row.getCell(Constants.XLS_EXCEPTED_RESULT).toString().trim().replace("\n", " & ");
-				     
+				     String title = "";
+				     String type = "";
+				     String priority = "";
+				     String excepted_result = "";
+				     Cell cell = row.getCell(Constants.XLS_TITLE);
+				     if(cell != null)
+				    	 title = cell.toString().trim();
 				     if(title.isEmpty())
 				    	 continue;
+				     
+				     cell = row.getCell(Constants.XLS_TYPE);
+				     if(cell != null)
+				    	 type = cell.toString().trim();
+				     cell = row.getCell(Constants.XLS_PRIORITY);
+				     if(cell != null)
+				    	 priority = cell.toString().trim();
+				     cell = row.getCell(Constants.XLS_EXCEPTED_RESULT);
+				     if(cell != null)
+				    	 excepted_result = cell.toString().trim().replace("\n", " & ");
+				     
 				     if(type.isEmpty() && priority.isEmpty() && excepted_result.isEmpty()){
 				    	 descript_head = title;
 				    	 continue;
